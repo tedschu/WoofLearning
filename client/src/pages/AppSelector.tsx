4a6d76c9b7ff4431e4d05fcdc173c8f931f6fd3d
tedschu <ted.schuster@gmail.com>
2024-12-06 14:29:39 -0600

    adding initial styling for app page

--- client/src/pages/AppSelector.tsx
+++ client/src/pages/AppSelector.tsx
@@ -1,7 +1,6 @@
 // Nav screen to choose math or reading
 // Default landing page for logged-in users
 
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
@@ -20,13 +19,19 @@ function AppSelector() {
 
   return (
     <>
-      <Link to={"/game-math"} className="welcomePrivacyNotice">
-        Math
-      </Link>
-      <br />
-      <Link to={"/game-reading"} className="welcomePrivacyNotice">
-        Reading
-      </Link>
+      <div className="appSelector-Wrapper">
+        <div className="appSelector-ContentContainer">
+          <div className="appSelector-contentBox">Woof Reading</div>
+          <div className="appSelector-contentBox">Woof Math</div>
+          {/* <Link to={"/game-math"} className="welcomePrivacyNotice">
+              Math
+            </Link>
+            <br />
+            <Link to={"/game-reading"} className="welcomePrivacyNotice">
+              Reading
+            </Link> */}
+        </div>
+      </div>
     </>
   );
 }
