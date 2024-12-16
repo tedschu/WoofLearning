0438839b06f5298124c677bd9406d4f3f668577b
tedschu <ted.schuster@gmail.com>
2024-12-16 11:18:56 -0600

    increased size of images on help modal

--- client/src/components/ChartModal.tsx
+++ client/src/components/ChartModal.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import mathImage from "./../assets/helpPopup-math.png";
 import readingImage from "./../assets/helpPopup-reading.png";
 import chartImage from "./../assets/helpPopup-chart.png";
+import { Link } from "react-router-dom";
 
 type ChartModalTypes = {
   isChartModalOpen: boolean;
@@ -20,7 +21,14 @@ function ChartModal({
     <>
       <div className="modalOverlay-help">
         <div className="modalContent-help">
-          <h2>What are these charts?</h2>
+          <div className="modalContent-topLine">
+            <Link to="#" onClick={closeChartModal}>
+              <h3 className="grid-firstItem">X</h3>
+            </Link>
+
+            <h2>What are these charts?</h2>
+            <h2></h2>
+          </div>
           <p>
             They're showing you how many points you've earned at the different
             "how hard should the questions be?" levels.
